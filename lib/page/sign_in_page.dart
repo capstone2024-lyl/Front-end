ba0c67a1aeb9979e979cae66808136baa10d48ad
lee0jae330 <xingxing2001@cau.ac.kr>
2024-05-05 18:46:41 +0900

    feat: textFormField 외 영역 터치 시 키보드 내려가게 하는 기능 추가

--- lib/page/sign_in_page.dart
+++ lib/page/sign_in_page.dart
@@ -19,183 +19,188 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        physics: NeverScrollableScrollPhysics(),
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 75,
-            ),
-            const Center(
-              child: Text(
-                'I Card: 나를 알고, 소개하다',
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w500,
-                ),
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(FocusNode());
+      },
+      child: Scaffold(
+        body: SingleChildScrollView(
+          physics: NeverScrollableScrollPhysics(),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 75,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Image.asset(
-              'assets/images/loading_img.png',
-              width: 200,
-              height: 200,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 20.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
+              const Center(
                 child: Text(
-                  '로그인',
+                  'I Card: 나를 알고, 소개하다',
                   style: TextStyle(
-                    fontSize: 22,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              width: 380,
-              height: 60,
-              child: TextField(
-                controller: _idController,
-                decoration: InputDecoration(
-                    labelText: '아이디',
-                    hintText: '아이디를 입력하세요',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5.0),
-                    )),
+              const SizedBox(
+                height: 10,
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            Container(
-              width: 380,
-              height: 60,
-              child: TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                    labelText: '비밀번호',
-                    hintText: '비밀번호를 입력하세요',
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0))),
+              Image.asset(
+                'assets/images/loading_img.png',
+                width: 200,
+                height: 200,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            SizedBox(
-              width: 380,
-              height: 60,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: AppColor.buttonColor.getColor(),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-                onPressed: () {},
-                child: const Text(
-                  '로그인',
-                  style: TextStyle(fontSize: 24, color: Colors.white),
-                ),
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                TextButton(
-                  //TODO 아이디 찾기 page로 이동
-                  onPressed: () {},
-                  child: const Text(
-                    '아이디 찾기',
+              const Padding(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    '로그인',
                     style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black,
+                      fontSize: 22,
                     ),
                   ),
                 ),
-                TextButton(
-                  //TODO 비밀번호 찾기 page로 이동
-                  onPressed: () {},
-                  child: const Text(
-                    '비밀번호 찾기',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-                TextButton(
-                  //TODO 회원가입 page로 이동
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => SignUpPage()));
-                  },
-                  child: const Text(
-                    '회원가입',
-                    style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Transform.translate(
-              offset: const Offset(0, -10),
-              child: const Divider(
-                indent: 20,
-                endIndent: 20,
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              'SNS 로그인',
-              style: TextStyle(
-                fontSize: 18,
+              SizedBox(
+                width: 380,
+                height: 60,
+                child: TextField(
+                  controller: _idController,
+                  decoration: InputDecoration(
+                      labelText: '아이디',
+                      hintText: '아이디를 입력하세요',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      )),
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              width: 380,
-              height: 60,
-              child: ElevatedButton(
-                //TODO 구글 로그인 구현
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.black,
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: 380,
+                height: 60,
+                child: TextField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                      labelText: '비밀번호',
+                      hintText: '비밀번호를 입력하세요',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: 380,
+                height: 60,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.buttonColor.getColor(),
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    side: const BorderSide(
-                      color: Colors.black38,
-                      width: 1,
-                    )),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SvgPicture.asset(
-                      'assets/icons/google_icon.svg',
-                      width: 50,
-                      height: 50,
-                    ),
-                    const Text(
-                      '구글 계정으로 로그인',
-                      style: TextStyle(fontSize: 24),
-                    )
-                  ],
+                  ),
+                  onPressed: () {},
+                  child: const Text(
+                    '로그인',
+                    style: TextStyle(fontSize: 24, color: Colors.white),
+                  ),
                 ),
               ),
-            ),
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  TextButton(
+                    //TODO 아이디 찾기 page로 이동
+                    onPressed: () {},
+                    child: const Text(
+                      '아이디 찾기',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    //TODO 비밀번호 찾기 page로 이동
+                    onPressed: () {},
+                    child: const Text(
+                      '비밀번호 찾기',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    //TODO 회원가입 page로 이동
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => SignUpPage()));
+                    },
+                    child: const Text(
+                      '회원가입',
+                      style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Transform.translate(
+                offset: const Offset(0, -10),
+                child: const Divider(
+                  indent: 20,
+                  endIndent: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'SNS 로그인',
+                style: TextStyle(
+                  fontSize: 18,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              SizedBox(
+                width: 380,
+                height: 60,
+                child: ElevatedButton(
+                  //TODO 구글 로그인 구현
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                      side: const BorderSide(
+                        color: Colors.black38,
+                        width: 1,
+                      )),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/google_icon.svg',
+                        width: 50,
+                        height: 50,
+                      ),
+                      const Text(
+                        '구글 계정으로 로그인',
+                        style: TextStyle(fontSize: 24),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
