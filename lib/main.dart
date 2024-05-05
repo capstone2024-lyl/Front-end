2bdc1c0ed18de65301eb9eb34ed35f62d75a49cf
lee0jae330 <xingxing2001@cau.ac.kr>
2024-05-05 20:53:14 +0900

    feat: 기본 폰트 변경 (프리텐다드), datePicker 기본언어 한글로 설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:untitled1/page/home_page.dart';
@@ -8,7 +10,10 @@ import 'package:untitled1/page/loading_page.dart';
 import 'package:untitled1/util/app_color.dart';
 import 'package:untitled1/page/sign_in_page.dart';
 
+
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
@@ -18,17 +23,26 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
+      locale: const Locale('ko', 'KR'), // 한국어와 한국을 앱의 로케일로 설정
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('ko', 'KR'), // 한국어 지원
+      ],
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        fontFamily: 'DoHyeon',
+        fontFamily: 'Pretendard',
         useMaterial3: true,
       ),
       //home: HomePage(),
       home: SignInPage(),
+      //home: LoadingPage(),
     );
   }
 }
-
-
