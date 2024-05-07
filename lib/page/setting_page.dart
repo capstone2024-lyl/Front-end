cc3671860005a24c7e964987f1b972d569ebabb0
lee0jae330 <xingxing2001@cau.ac.kr>
2024-05-07 16:40:10 +0900

    feat: 환경 설정 페이지 생성

---
+++ lib/page/setting_page.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  const SettingPage({super.key});
+
+  @override
+  State<SettingPage> createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          '환경 설정 페이지입니다.'
+        ),
+      ),
+    );
+  }
+}
