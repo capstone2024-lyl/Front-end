d67c4c43f7ef4e9fefda1d24491b6102c073ae8d
leeyeongjae <xingxing2001@cau.ac.kr>
2024-05-21 03:07:33 +0900

    feat: 앱 사용기록 접근 권한이 거절 됐을 시, 알림창 생성 및 사용자가 권한을 수락할 수 있게끔 기능 추가

--- android/app/src/main/kotlin/com/example/untitled1/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/untitled1/MainActivity.kt
@@ -1,6 +1,36 @@
 package com.example.untitled1
 
+import android.app.AppOpsManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.example.untitled1/usage_access"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if (call.method == "checkUsageAccess") {
+                result.success(hasUsageStatsPermission())
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun hasUsageStatsPermission(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val appOps = getSystemService(Context.APP_OPS_SERVICE) as AppOpsManager
+            val mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                    android.os.Process.myUid(), packageName)
+            return mode == AppOpsManager.MODE_ALLOWED
+        }
+        return false
+    }
 }
