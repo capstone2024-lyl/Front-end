c8d127ece730b209825732ca63b9a337153017fb
lee0jae330 <xingxing2001@cau.ac.kr>
2024-05-24 11:58:38 +0900

    feat: 서버로 파일 업로드 시 로딩 화면 추가

--- lib/page/chat_analyze_intro_page.dart
+++ lib/page/chat_analyze_intro_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:untitled1/page/chat_analyze_result_page.dart';
@@ -12,6 +13,7 @@ import 'package:untitled1/util/app_color.dart';
 
 class ChatAnalyzeIntroPage extends StatefulWidget {
   final VoidCallback onNavigateToProfile;
+
   const ChatAnalyzeIntroPage({super.key, required this.onNavigateToProfile});
 
   @override
@@ -19,13 +21,12 @@ class ChatAnalyzeIntroPage extends StatefulWidget {
 }
 
 class _ChatAnalyzeIntroPageState extends State<ChatAnalyzeIntroPage> {
-
   final ApiService _apiService = ApiService();
 
   final PageController _pageController = PageController();
   int _currentPage = 0;
 
-  String? _fileName;
+  bool _isLoading = false;
 
   @override
   void initState() {
@@ -89,6 +90,10 @@ class _ChatAnalyzeIntroPageState extends State<ChatAnalyzeIntroPage> {
   }
 
   Future<void> _pickTextFile() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['txt'],
@@ -97,13 +102,13 @@ class _ChatAnalyzeIntroPageState extends State<ChatAnalyzeIntroPage> {
     if (result != null) {
       PlatformFile platformFile = result.files.first;
       File file = File(platformFile.path!);
-      String path = file.path;
-      bool requestResult =
-          await _apiService.uploadChattingFile('\\storage\\emulated\\0\\Download\\KakaoTalk', file);
+      bool requestResult = await _apiService.uploadChattingFile(file);
       if (requestResult) {
         if (mounted) {
           Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => ChatAnalyzeResultPage(onNavigateToProfile: widget.onNavigateToProfile,)));
+              builder: (context) => ChatAnalyzeResultPage(
+                    onNavigateToProfile: widget.onNavigateToProfile,
+                  )));
         }
       }
     }
@@ -112,131 +117,145 @@ class _ChatAnalyzeIntroPageState extends State<ChatAnalyzeIntroPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          const SizedBox(
-            height: 50,
-          ),
-          const Center(
-            child: Text(
-              '채팅을 통한 MBTI 분석하기',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Stack(
+        children: [
+          Column(
+            children: <Widget>[
+              const SizedBox(
+                height: 50,
               ),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Container(
-            width: 380,
-            height: 500,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.7),
-                  blurRadius: 3.0,
-                  spreadRadius: 0.0,
-                  offset: const Offset(0.0, 5.0),
-                ),
-              ],
-            ),
-            child: Column(
-              children: <Widget>[
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  '채팅 업로드 방법',
+              const Center(
+                child: Text(
+                  '채팅을 통한 MBTI 분석하기',
                   style: TextStyle(
-                    fontSize: 26,
+                    fontSize: 24,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-                const Divider(
-                  indent: 20,
-                  endIndent: 20,
-                ),
-                SizedBox(
-                  width: 350,
-                  height: 300,
-                  child: PageView(
-                    controller: _pageController,
-                    children: [
-                      SvgPicture.asset('assets/images/chat1.svg'),
-                      SvgPicture.asset('assets/images/chat2.svg'),
-                      SvgPicture.asset('assets/images/chat3.svg'),
-                      SvgPicture.asset('assets/images/chat4.svg'),
-                    ],
-                  ),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _buildIndicator(0),
-                    const SizedBox(
-                      width: 5,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 380,
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.7),
+                      blurRadius: 3.0,
+                      spreadRadius: 0.0,
+                      offset: const Offset(0.0, 5.0),
                     ),
-                    _buildIndicator(1),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    _buildIndicator(2),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    _buildIndicator(3),
                   ],
                 ),
-                const SizedBox(
-                  height: 10,
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      '채팅 업로드 방법',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Divider(
+                      indent: 20,
+                      endIndent: 20,
+                    ),
+                    SizedBox(
+                      width: 350,
+                      height: 300,
+                      child: PageView(
+                        controller: _pageController,
+                        children: [
+                          SvgPicture.asset('assets/images/chat1.svg'),
+                          SvgPicture.asset('assets/images/chat2.svg'),
+                          SvgPicture.asset('assets/images/chat3.svg'),
+                          SvgPicture.asset('assets/images/chat4.svg'),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        _buildIndicator(0),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        _buildIndicator(1),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        _buildIndicator(2),
+                        const SizedBox(
+                          width: 5,
+                        ),
+                        _buildIndicator(3),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Step ${_currentPage + 1}',
+                      style: const TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    _buildStepText(_currentPage),
+                  ],
                 ),
-                Text(
-                  'Step ${_currentPage + 1}',
-                  style: const TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.bold,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                '채팅 내역은 분석에만 사용되고 바로 삭제돼요!',
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SizedBox(
+                width: 380,
+                height: 60,
+                child: ElevatedButton(
+                  onPressed: _requestPermissionAndPickFile,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColor.buttonColor.colors,
+                    foregroundColor: Colors.white,
+                  ),
+                  child: const Text(
+                    '채탱 내역 업로드 하기',
+                    style: TextStyle(
+                      fontSize: 26,
+                    ),
                   ),
                 ),
-                _buildStepText(_currentPage),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          const Text(
-            '채팅 내역은 분석에만 사용되고 바로 삭제돼요!',
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          SizedBox(
-            width: 380,
-            height: 60,
-            child: ElevatedButton(
-              onPressed: _requestPermissionAndPickFile,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: AppColor.buttonColor.colors,
-                foregroundColor: Colors.white,
               ),
-              child: const Text(
-                '채탱 내역 업로드 하기',
-                style: TextStyle(
-                  fontSize: 26,
+            ],
+          ),
+          if (_isLoading)
+            Positioned.fill(
+              child: Container(
+                color: Colors.black54,
+                child: SpinKitWaveSpinner(
+                  color: AppColor.buttonColor.colors,
+                  size: 150,
                 ),
               ),
             ),
-          ),
         ],
       ),
     );
